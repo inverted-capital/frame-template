0a24d50a2143172e50576039f4cdd42d169fa30f
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2025-05-16 16:32:20 +1200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import AccountView from './views/AccountView';
 function App() {
   return (
     <div className="min-h-screen">
-      <div className="container mx-auto py-8 px-4 max-w-7xl">
+      <div className="container mx-auto  max-w-7xl">
         <AccountView />
       </div>
     </div>
