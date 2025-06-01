f8c03408427625257f9f562407667cefa3e40010
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2025-06-01 05:06:10 +0000

    add ssl cert for localhost

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), mkcert()],
   optimizeDeps: {
     exclude: ['lucide-react']
   },
   build: {
     sourcemap: true
   },
-  base: './'
+  base: './',
+  server: {
+    https: true
+  }
 })
